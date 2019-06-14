ac1dc32c7a643e940b344ed45fd716dfffbb5a3f
Graeme Hoffman <graeme@graemehoffman.com>
2019-06-14 17:08:48 +0200

    Cleanuop

--- diviner-archive-plugin.php
+++ diviner-archive-plugin.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * Dashboard. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
  *
  * @link              https://ncpr.github.io/diviner-wp-archive-theme/
  * @since             0.1.0
