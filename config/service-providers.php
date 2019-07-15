ecab4a147a81ef6860c5f2caba547b99183f2a75
Graeme Hoffman <graeme@graemehoffman.com>
2019-07-15 16:14:58 +0200

    Add select field and initial cache hydration

--- config/service-providers.php
+++ config/service-providers.php
@@ -132,6 +132,9 @@ return [
     'carbonfields.boot' => [
 	    'class' => NCPR\DivinerArchivePlugin\CarbonFields\Boot::class,
     ],
+    'post_types.diviner_field.diviner_field' => [
+	    'class' => NCPR\DivinerArchivePlugin\CPT\Diviner_Field\Diviner_Field::class,
+    ],
     'post_types.diviner_field.postmeta' => [
 	    'class' => NCPR\DivinerArchivePlugin\CPT\Diviner_Field\PostMeta::class,
     ],
